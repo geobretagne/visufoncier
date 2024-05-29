9093a1c9f60793922e43d6f604b3edfcde6022b1
mgtlbn <margot.leborgne@gmail.com>
2024-05-29 13:17:06 +0200

    v0.3

--- requete_flux.sql
+++ requete_flux.sql
@@ -88,3 +88,46 @@ FROM visufoncier.nomenclature_mos
 WHERE visufoncier.mos_foncier_agrege_temp.code4_2021 = visufoncier.nomenclature_mos.code_n4;
 
 -- ==========================================================
+--Calcul des flux
+
+-- Ajouter deux nouvelles colonnes à la table "mos_foncier_agrege_temp" dans le schéma "visufoncier"
+ALTER TABLE visufoncier.mos_foncier_agrege_temp
+ADD COLUMN enaf_conso_2021 text,
+ADD COLUMN artificialisees_2021 text;
+
+-- Mettre à jour les valeurs des nouvelles colonnes à partir de la table "nomenclature_mos"
+UPDATE visufoncier.mos_foncier_agrege_temp
+SET
+    enaf_conso_2021 = nomenclature_mos.enaf_conso,
+    artificialisees_2021 = nomenclature_mos.artificialisees
+FROM visufoncier.nomenclature_mos
+WHERE visufoncier.mos_foncier_agrege_temp.code4_2021 = visufoncier.nomenclature_mos.code_n4;
+
+
+-- Ajouter une nouvelle colonne nommée flux_conso dans la table. Si flux_conso=1 alors flux de consommation.
+ALTER TABLE visufoncier.mos_foncier_agrege_temp
+ADD COLUMN flux_conso integer;
+
+-- Calcul si oui ou non flux en mettant à jour la colonne flux_conso
+UPDATE visufoncier.mos_foncier_agrege_temp
+SET flux_conso = CASE WHEN enaf_conso_2011 = 'enaf' AND enaf_conso_2021 = 'conso' THEN 1 ELSE 0 END;
+
+
+-- Ajouter une nouvelle colonne nommée flux_artif dans la table. Si flux_artif=1 alors flux d'artificialisation.
+ALTER TABLE visufoncier.mos_foncier_agrege_temp
+ADD COLUMN flux_artif integer;
+
+-- Calcul si oui ou non flux en mettant à jour la colonne flux_conso
+UPDATE visufoncier.mos_foncier_agrege_temp
+SET flux_artif = CASE WHEN artificialisees_2011 = 'non artificialisees' AND (artificialisees_2021 = 'artificialisees' OR artificialisees_2021 = 'artificialisees en partie') THEN 1 ELSE 0 END;
+
+
+-- Ajouter une nouvelle colonne nommée flux_conso dans la table. Si flux_conso=1 alors flux de consommation.
+ALTER TABLE visufoncier.mos_foncier_agrege_temp
+ADD COLUMN flux_renaturation integer;
+
+-- Calcul si oui ou non flux en mettant à jour la colonne flux_conso
+UPDATE visufoncier.mos_foncier_agrege_temp
+SET flux_renaturation = CASE WHEN enaf_conso_2021 = 'enaf' AND enaf_conso_2011 = 'conso' THEN 1 ELSE 0 END;
+
+-- ==========================================================
